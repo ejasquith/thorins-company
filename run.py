88042328b64a83e885b991436a48019fca3fbf1d
ejasquith <emily.asquith@icloud.com>
2022-08-13 02:28:20 +0000

    Render individual member about pages

--- run.py
+++ run.py
@@ -13,9 +13,20 @@ def index():
 @app.route("/about")
 def about():
     data = []
-    with open("data/company.json", "r") as json_data:
+    with open("data/company.json", "r", encoding="utf-8") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r", encoding="utf-8") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member, page_title=member["name"])
 
 
 @app.route("/contact")
@@ -27,5 +38,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
+        debug=True,
     )
